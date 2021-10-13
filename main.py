6a56d8e2e6fb836b20d5943eb15efffa764d69cd
Geoff Whittington <geoff.whittington@gmail.com>
2021-10-13 11:05:13 -0400

    Add some checks for missing environment variables

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ import requests
 
 
 def main():
+    if "INPUT_CAMPAIGNSECRET" not in os.environ:
+        raise Exception("Missing INPUT_CAMPAIGNSECRET")
+    if "INPUT_USERID" not in os.environ:
+        raise Exception("Missing INPUT_USERID")
+
     campaign_secret = os.environ["INPUT_CAMPAIGNSECRET"]
     user_id = os.environ["INPUT_USERID"]
-    platform = os.environ["INPUT_PLATFORM"]
-    amount = os.environ["INPUT_AMOUNT"]
-    server = os.environ["INPUT_SERVER"]
+
+    if "INPUT_AMOUNT" in os.environ:
+        amount = os.environ["INPUT_AMOUNT"]
+    else:
+        amount = 1
+
+    if "INPUT_PLATFORM" in os.environ:
+        platform = os.environ["INPUT_PLATFORM"]
+    else:
+        platform = "github"
+
+    if "INPUT_SERVER" in os.environ:
+        server = os.environ["INPUT_SERVER"]
+    else:
+        server = "https://perk.exchange"
 
     r = requests.post(
         "{}/api/rewards".format(server),
